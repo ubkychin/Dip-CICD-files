ecb562050ec436013d12bd0e678c4d8aaa0e92b1
Anh Nguyen <anguyen@swin.edu.au>
2019-04-08 16:07:27 +1000

    Create Room.cs

---
+++ Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleWebApplication.Models {
+    public class Room {
+        public int Number;
+        public string Building;
+        public int Capacity;
+
+        public Room() {
+            Number = 224;
+            Building = "TD";
+            Capacity = 22;
+        }
+
+        public Room(int number, string building, int capacity) {
+            Number = number;
+            Building = building;
+            this.Capacity = capacity;
+        }
+
+        public string getInfo() {
+            return $"{Building}{Number} - {Capacity}";
+        }
+    }
+}
